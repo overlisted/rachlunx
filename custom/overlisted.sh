fb72eae5af33dc7eec90d52018537b38c9bf7cca
Ilya Maximov <mail@overlisted.net>
2022-04-18 18:43:15 +0200

    overlisted.sh: add dconf

--- custom/overlisted.sh
+++ custom/overlisted.sh
@@ -23,6 +23,7 @@ function custom_post() {
 	cd /tmp
 	git clone --recurse-submodules https://github.com/overlisted/dotfiles
 	rsync -a dotfiles/files/.* $HOME
+	dconf load /. < settings.dconf
 
 	rustup default nightly
 	rustup component add rustfmt
